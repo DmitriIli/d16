a805337b61910e15aebb151b3aacfefa3c58108c
dili <di.grebenev@outlook.com>
2023-02-16 16:59:54 +0300

    models

--- board/board/settings.py
+++ board/board/settings.py
@@ -9,12 +9,15 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale')
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -87,18 +90,18 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    # },
 ]
 
 
@@ -117,9 +120,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+SITE_ID = 1
+
+AUTH_USER_MODEL = 'auth.User'
--- board/main/models.py
+++ board/main/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Sum
+from datetime import datetime
+from django.urls import reverse, reverse_lazy
+from django.core.cache import cache
+
+from board import settings
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
+    is_activate = models.BooleanField(default=False)
+
+class Categories(models.Model):
+    name = models.CharField(max_length=64)
+
+class Ads(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    time_create = models.DateTimeField(auto_created=True)
+    title = models.CharField(max_length=64, default=f'заголовок статьи от {datetime.now()}')
+    text = models.CharField(max_length=512, default=f'текст статьи от {datetime.now()}')
+    category = models.ForeignKey(Categories, on_delete=models.DO_NOTHING)
+    upload = models.FileField(upload_to='media/')
+
+
+class FeedBack(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    ads = models.ForeignKey(Ads, on_delete=models.CASCADE)
+    time_create = models.DateField(auto_created=True)
+    text = models.CharField(max_length=256, default=f'текст отклика от {datetime.now()}')
